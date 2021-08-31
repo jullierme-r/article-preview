a64f13b76f81e9f9a1520c3cb46761275384cee9
Jullierme Rodrigues Gomes <jullierme_r@hotmail.com>
2021-08-31 15:56:24 -0400

    Add files via upload

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,16 +1,21 @@
 "use strict";
 
 //selects the elements that will be modified
-let btnShare = document.getElementById("share");
-let bgShare = document.querySelector(".share-img")
+let btnShare = document.querySelector(".share-btn");
 let profileBlock = document.querySelector(".profile");
 let socialDiv = document.querySelector(".social");
 let contactDiv = document.querySelector(".contact");
 
 //when clicked adds/removes the classes necessary to the effects
-btnShare.addEventListener("click", ()=> { 
-    bgShare.classList.toggle("bg-share-img");
+btnShare.addEventListener("click", ()=> {
+    if(btnShare.ariaExpanded == "false") {
+        btnShare.ariaExpanded = "true";
+    } else {
+        btnShare.ariaExpanded = "false";
+    }
+    btnShare.classList.toggle("bg-share-btn");
     profileBlock.classList.toggle("active");
     socialDiv.classList.toggle("show-social");
     contactDiv.classList.toggle("hide-contact");
 });
+
